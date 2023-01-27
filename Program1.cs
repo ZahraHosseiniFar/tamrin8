484cb6b8d31838d8423921ba2d45f6ffc2a1a032
ZahraHosseiniFar <117304346+ZahraHosseiniFar@users.noreply.github.com>
2023-01-27 20:30:43 +0330

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+
+            csl test = new csl();
+            test.a =Convert.ToInt32(Console.ReadLine());
+            test.b =Convert.ToInt32(Console.ReadLine());
+            test.c =Convert.ToInt32(Console.ReadLine());
+            test.d= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Convert.ToDouble(test.mlt()));
+            Console.WriteLine(Convert.ToDouble(test.min()));
+            Console.WriteLine(Convert.ToDouble(test.sum()));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ csl.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    class csl
+    {
+        private int A;
+        private int B;
+        private int C;
+        private int D;
+
+        public int a
+        {
+            get { return A; }
+            set { A = value; }
+        }
+        public int b
+        {
+            get { return B; }
+            set { B = value; }
+        }
+        public int c
+        {
+            get { return C; }
+            set { C = value; }
+        }
+        public int d
+        {
+            get { return D; }
+            set { D = value; }
+
+        }
+        public void cal() { }
+        public bool cal(int a, int b, int c, int d)
+
+        {
+            if (b != 0 && d != 0)
+            {
+                A = a;
+                B = b;
+                C = c;
+                D = d;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double mlt()
+        {
+            return ((A / B) * (C/D));
+        }
+        public double min()
+        {
+            return ((A/B) - (C/D));
+        }
+        public double sum()
+        {
+            return ((A / B) + (C / D));
+        }
+    }
+}
